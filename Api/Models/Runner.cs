5a54fc71bab7bb6072b9cb4c04139376a14fcd3f
Sydney March <sydneymarch@Sydneys-MacBook-Pro-2.local>
2025-04-21 00:11:56 -0500

    woop

--- Api/Models/Runner.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Models
-{
-    public class Runner
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-    }
-}
